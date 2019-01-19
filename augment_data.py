bcddafb068f30b267432aeefba2eea9a7facab9c
LongGang Pang <lgpang@qq.com>
2019-01-19 12:20:44 -0800

    update augment_data.py to take into account the augmentation when there is no speaker in the original context

--- augment_data.py
+++ augment_data.py
@@ -19,18 +19,27 @@ def data_augmentation(saved_file="label_honglou.txt"):
     uid = 0   
     len_truncate = 128
     for speaker in speakers:
-        for ctx in contexts:
+        for ctx_left, ctx_right, istart in contexts:
+            ### do not create new sentence for contexts without original speaker
+            if istart == -1: continue
             try:
-                new_ctx = ctx[0] + speaker + ctx[1]
-                istart = ctx[2]
+                new_ctx = ctx_left + speaker + ctx_right
                 new_iend = istart + len(speaker)
                 new_speaker = speaker
-                # truncate the input if the speaker is contained in the last 128 words
-                if len(new_ctx) > len_truncate and (len(new_ctx)-istart)<len_truncate:
+
+                # truncate the input if sentence is longer than 128 words
+                if len(new_ctx) > len_truncate:
                     truncated_ctx = new_ctx[-len_truncate:]
-                    istart = ctx[2] - (len(new_ctx) - len_truncate)
-                    new_iend = istart + len(speaker)
-                    new_speaker = truncated_ctx[istart:new_iend]
+                    # if speaker is in the truncated_ctx
+                    if (len(new_ctx)-istart)<len_truncate:
+                        istart = istart - (len(new_ctx) - len_truncate)
+                        new_iend = istart + len(speaker)
+                        new_speaker = truncated_ctx[istart:new_iend]
+                    else: # if speaker is not in the truncated_ctx
+                        istart = -1
+                        new_iend = 0
+                        new_speaker = None
+                    new_ctx = truncated_ctx
                 res = {'uid':uid, 
                    'context':new_ctx,
                    'speaker':new_speaker,
---
+++ dataset.py
@@ -0,0 +1,31 @@
+from augment_data import speakers
+import random
+
+random.shuffle(speakers)
+
+ntrain = 360000
+nvalid = 12000
+ntest  = 12000
+
+def construct_dataset(kind='training', start_idx=0, end_idx=ntrain):
+    xtrain = []
+    ytrain = []
+    for speaker in speakers[:ntrain]:
+        ctx = speaker['context']
+        xtrain.append(speaker['context'])
+        res = ctx[speaker['istart']: speaker['iend']]
+        ytrain.append([speaker['istart'], speaker['iend'], res])
+    
+    with open("%s_sentence.csv"%kind, "w") as fxtrain:
+        for x in xtrain:
+            fxtrain.write(x)
+            fxtrain.write('\n')
+    
+    with open("%s_labels.csv"%kind, "w") as fytrain:
+        for y in ytrain:
+            fytrain.write('%s ||| %s ||| %s'%(y[0], y[1], y[2]))
+            fytrain.write('\n')
+
+construct_dataset(kind='training', start_idx=0, end_idx=ntrain)
+construct_dataset(kind='validation', start_idx=ntrain, end_idx=ntrain+nvalid)
+construct_dataset(kind='testing', start_idx=ntrain+nvalid, end_idx=ntrain+nvalid+ntest)
