b0cf33e90eb534326b09655a9590a0f1abf5ca2c
LongGang Pang <lgpang@qq.com>
2019-01-22 15:29:00 -0800

    update with squad dataset preparison and predictions from bert

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ ntrain = 360000
 nvalid = 12000
 ntest  = 12000
 
-def construct_dataset(kind='training', start_idx=0, end_idx=ntrain):
+def construct_dataset(kind='training', start_idx=0, end_idx=ntrain, save_to_disk=True):
     xtrain = []
     ytrain = []
     for speaker in speakers[:ntrain]:
@@ -16,16 +16,19 @@ def construct_dataset(kind='training', start_idx=0, end_idx=ntrain):
         res = ctx[speaker['istart']: speaker['iend']]
         ytrain.append([speaker['istart'], speaker['iend'], res])
     
-    with open("%s_sentence.csv"%kind, "w") as fxtrain:
-        for x in xtrain:
-            fxtrain.write(x)
-            fxtrain.write('\n')
-    
-    with open("%s_labels.csv"%kind, "w") as fytrain:
-        for y in ytrain:
-            fytrain.write('%s ||| %s ||| %s'%(y[0], y[1], y[2]))
-            fytrain.write('\n')
+    if save_to_disk:
+        with open("%s_sentence.csv"%kind, "w") as fxtrain:
+            for x in xtrain:
+                fxtrain.write(x)
+                fxtrain.write('\n')
+        
+        with open("%s_labels.csv"%kind, "w") as fytrain:
+            for y in ytrain:
+                fytrain.write('%s ||| %s ||| %s'%(y[0], y[1], y[2]))
+                fytrain.write('\n')
 
-construct_dataset(kind='training', start_idx=0, end_idx=ntrain)
-construct_dataset(kind='validation', start_idx=ntrain, end_idx=ntrain+nvalid)
-construct_dataset(kind='testing', start_idx=ntrain+nvalid, end_idx=ntrain+nvalid+ntest)
+    return xtrain, ytrain
+
+xtrain, ytrain = construct_dataset(kind='training', start_idx=0, end_idx=ntrain)
+xvalid, yvalid = construct_dataset(kind='validation', start_idx=ntrain, end_idx=ntrain+nvalid)
+xtest,  ytest  = construct_dataset(kind='testing', start_idx=ntrain+nvalid, end_idx=ntrain+nvalid+ntest)
